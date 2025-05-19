6bdd67dcf19e5f5c344daf26f9f14e0e084a3e9c
hayk-m-h <h.m.mnatsakanyan@gmail.com>
2025-05-20 01:26:25 +0400

    section_2_2

--- reactivites/API/Program.cs
+++ reactivites/API/Program.cs
@@ -1,12 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+
+try
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedDataAsync(context);
+}
+catch (Exception e)
+{
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e,  "An error occurred while migrating or seeding the database.");
+}
 
 app.Run();
