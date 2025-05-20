0304f3f02900b5b1731447d40c8767f9d677ace8
hayk-m-h <h.m.mnatsakanyan@gmail.com>
2025-05-20 19:30:08 +0400

    section_3_1

--- reactivites/API/Program.cs
+++ reactivites/API/Program.cs
@@ -9,9 +9,13 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
