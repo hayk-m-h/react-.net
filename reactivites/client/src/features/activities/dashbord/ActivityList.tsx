a9d63262fe0b87210553e206fa8def3d590562ad
hayk-m-h <h.m.mnatsakanyan@gmail.com>
2025-08-11 11:07:05 +0400

    section_5_2

--- reactivites/client/src/features/activities/dashbord/ActivityList.tsx
+++ reactivites/client/src/features/activities/dashbord/ActivityList.tsx
@@ -3,14 +3,20 @@ import ActivityCard from "./ActivityCard";
 
 type Props = {
     activites: Activity[]
-    selectActivity: (id: string) =>void
+    selectActivity: (id: string) => void
+    deleteActivity: (id: string) => void
 }
 
-export default function ActivityList({activites, selectActivity} : Props) {
+export default function ActivityList({activites, selectActivity, deleteActivity} : Props) {
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', gap: 3}}>
         {activites.map(activite => (
-            <ActivityCard key={activite.id} activity={activite} selectActivity={selectActivity}/>
+            <ActivityCard 
+              key={activite.id} 
+              activity={activite} 
+              selectActivity={selectActivity}
+              deleteActivity={deleteActivity}
+              />
         ))}
     </Box>
   )
